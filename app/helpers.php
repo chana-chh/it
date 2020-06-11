4d8dcc2f77ec44fdddc504827d114288ddf018a3
kure-kg <stashakg@gmail.com>
2020-06-11 12:47:12 +0200

    Serveri ping 01

--- app/helpers.php
+++ app/helpers.php
@@ -2,7 +2,7 @@
 	
 	function ping($adresa)
 	{
-		$ping = exec("ping -n 1 $adresa", $izlaz, $status);
+		$ping = exec("ping -c 1 $adresa", $izlaz, $status);
 		if (strpos(implode(",", $izlaz), 'Destination host unreachable') !== false) {
 			return false;
     	} else {
